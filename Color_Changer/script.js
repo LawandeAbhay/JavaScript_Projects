c4a45081bb719b28050ef58c021bcda7bd1c0b80
LawandeAbhay <abhaylawande2002@gmail.com>
2025-06-27 22:27:06 +0530

    completed the color changer

---
+++ Color_Changer/script.js
@@ -0,0 +1,10 @@
+console.log("From color chnager");
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        const color = e.target.id;
+        body.style.backgroundColor = color;
+    });
+});
